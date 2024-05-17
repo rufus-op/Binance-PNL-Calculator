cb44feb682eec0d6a830e41399b7e5ba996051c8
Rauf <110355459+rufus-op@users.noreply.github.com>
2024-05-17 17:51:03 +0500

    changes

---
+++ lib/const/constants.dart
@@ -0,0 +1,6 @@
+// import 'package:google_fonts/google_fonts.dart';
+
+class AppConstants {
+  // Primary font for the app
+  // static String primaryFont = GoogleFonts.poppins().fontFamily.toString();
+}
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -17,113 +17,87 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xff090c22),
+      appBar: AppBar(
+        centerTitle: true,
         backgroundColor: const Color(0xff090c22),
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color(0xff090c22),
-          leading: InkWell(
-              onTap: () => Navigator.pop(context),
-              child: const Icon(
-                Icons.arrow_back_ios_new_outlined,
-                color: Colors.white,
-              )),
-          title: const Text(
-            'PNL (Profit and Loss)',
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
-          ),
+        leading: InkWell(
+            onTap: () => Navigator.pop(context),
+            child: const Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Colors.white,
+              size: 18,
+            )),
+        title: const Text(
+          'PNL (Profit and Loss)',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.w600, color: Colors.white),
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                children: [
-                  BannerAdWidget(adSize: AdSize.banner),
-                  Container(
-                    height: MediaQuery.of(context).size.height * .6,
-                    width: double.infinity,
-                    margin: const EdgeInsets.only(bottom: 20, top: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: LottieBuilder.asset(
-                            profit >= 0
-                                ? 'assets/profit-animation-green.json'
-                                : 'assets/loss.json',
-                            height: MediaQuery.of(context).size.height * .4,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                BannerAdWidget(adSize: AdSize.banner),
+                Container(
+                  height: MediaQuery.of(context).size.height * .6,
+                  width: double.infinity,
+                  margin: const EdgeInsets.only(bottom: 20, top: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: LottieBuilder.asset(
+                          profit >= 0
+                              ? 'assets/profit-animation-green.json'
+                              : 'assets/loss.json',
+                          height: MediaQuery.of(context).size.height * .4,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.35,
+                            child: const Text(
+                              'Profit/Loss',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.35,
-                              child: const Text(
-                                'Profit/Loss',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500),
-                              ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.25,
+                            child: Text(
+                              '${profit.toStringAsFixed(2)}\$',
+                              style: TextStyle(
+                                  color: profit >= 0.0
+                                      ? const Color(0xff44b581)
+                                      : Colors.red,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
                             ),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.25,
-                              child: Text(
-                                '${profit.toStringAsFixed(2)}\$',
-                                style: TextStyle(
-                                    color: profit >= 0.0
-                                        ? const Color(0xff44b581)
-                                        : Colors.red,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            ),
-                          ],
-                        ),
-                        if (isFutures) ...[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.35,
-                                child: const Text(
-                                  'Liquidation',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              ),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.25,
-                                child: Text(
-                                  '${liquidationPrice.toStringAsFixed(2)}\$',
-                                  style: const TextStyle(
-                                      color: Colors.red,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                              ),
-                            ],
                           ),
                         ],
+                      ),
+                      if (isFutures) ...[
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             SizedBox(
                               width: MediaQuery.of(context).size.width * 0.35,
                               child: const Text(
-                                'StopLoss',
+                                'Liquidation',
                                 style: TextStyle(
                                     color: Colors.black,
                                     fontSize: 18,
@@ -133,7 +107,7 @@ class ResultScreen extends StatelessWidget {
                             SizedBox(
                               width: MediaQuery.of(context).size.width * 0.25,
                               child: Text(
-                                '${stopLoss.toStringAsFixed(2)}\$',
+                                '${liquidationPrice.toStringAsFixed(2)}\$',
                                 style: const TextStyle(
                                     color: Colors.red,
                                     fontSize: 18,
@@ -143,35 +117,60 @@ class ResultScreen extends StatelessWidget {
                           ],
                         ),
                       ],
-                    ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.35,
+                            child: const Text(
+                              'StopLoss',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.25,
+                            child: Text(
+                              '${stopLoss.toStringAsFixed(2)}\$',
+                              style: const TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ],
                       ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        'Re-Calculate',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      )),
-                  const SizedBox(
-                    height: 8,
+                    ],
                   ),
-                  const SizedBox(
-                    height: 60,
-                    child: Text(
-                      'Use the code ”722626479” when opening your Binance Futures account and receive a 20% fee discount.',
-                      style: TextStyle(fontSize: 13, color: Colors.white),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                  )
-                ],
-              ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      'Re-Calculate',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    )),
+                const SizedBox(
+                  height: 8,
+                ),
+                const SizedBox(
+                  height: 60,
+                  child: Text(
+                    'Use the code ”722626479” when opening your Binance Futures account and receive a 20% fee discount.',
+                    style: TextStyle(fontSize: 13, color: Colors.white),
+                  ),
+                )
+              ],
             ),
           ),
         ),
